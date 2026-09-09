806d4f14dc923b9d041218be2d5d46a7f46c42bc
Amir <amir.azari.dev@gmail.com>
2026-09-09 07:40:50 -0700

    edit: edit schema db . delete name in users

--- prisma/migrations/20260909143932_init/migration.sql
+++ prisma/migrations/20260909143932_init/migration.sql
@@ -10,7 +10,6 @@ CREATE TYPE "booking_status" AS ENUM ('unconfirmed', 'checked-in', 'checked-out'
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
-    "name" VARCHAR(255),
     "email" VARCHAR(100) NOT NULL,
     "active" BOOLEAN NOT NULL DEFAULT true,
     "password" VARCHAR(255) NOT NULL,
